da8888c18aec85d05a82dea83ecd4b54ec128535
Ben Lomas <benlomas7@outlook.com>
2022-02-14 14:52:04 +0000

    Fixed merge errors

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,18 +1,15 @@
-
-const { updateArticle ,fetchArticle} = require("../models/articles");
+const { updateArticle, fetchArticle } = require("../models/articles");
 
 exports.patchArticle = (req, res, next) => {
   updateArticle(req.params.article_id, req.body)
-  then((article) => {
+    .then((article) => {
       res.status(200).send({ article });
     })
     .catch(next);
 };
 
-
 exports.getArticleById = (req, res, next) => {
   fetchArticle(req.params.article_id)
-
     .then((article) => {
       res.status(200).send({ article });
     })
--- models/articles.js
+++ models/articles.js
@@ -1,13 +1,12 @@
 const db = require("../db/connection");
 
-
 exports.updateArticle = (article_id, { inc_votes }) => {
   return db
     .query(
       "UPDATE articles SET votes=votes + $1 WHERE article_id= $2 RETURNING *",
       [inc_votes, article_id]
     )
-  .then(({ rows }) => {
+    .then(({ rows }) => {
       if (rows.length === 0)
         return Promise.reject({ status: 404, msg: "Not found" });
       return rows[0];
@@ -17,7 +16,6 @@ exports.updateArticle = (article_id, { inc_votes }) => {
 exports.fetchArticle = (article_id) => {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
-
     .then(({ rows }) => {
       if (rows.length === 0)
         return Promise.reject({ status: 404, msg: "Not found" });
