937784b5aecfd6198f7c359adac99baef98801fd
Ben Lomas <benlomas7@outlook.com>
2022-02-18 12:15:46 +0000

    Removed old comment code

--- app.js
+++ app.js
@@ -11,24 +11,6 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", apiRouter);
-// //API
-// app.get("/api", getEndpoints);
-
-// //TOPICS
-// app.get("/api/topics", getTopics);
-
-// //ARTICLES
-// app.get("/api/articles", getArticles);
-// app.get("/api/articles/:article_id", getArticleById);
-// app.patch("/api/articles/:article_id", patchArticle);
-
-// //USERS
-// app.get("/api/users", getUsers);
-
-// //COMMENTS
-// app.get("/api/articles/:article_id/comments", getCommentsByArticle);
-// app.post("/api/articles/:article_id/comments", postCommentByArticle);
-// app.delete("/api/comments/:comment_id", deleteCommentById);
 
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Path not found" });
